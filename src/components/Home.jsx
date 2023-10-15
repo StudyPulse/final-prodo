1a2a00eb091e3cdc38ed36b3c17b4ace39cc117b
Aarya Paradkar <aaru7276@gmail.com>
2023-10-15 20:33:45 +0530

    added kanban

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ import Notes from "./Notes";
 import Calendar from './Calendar';
 import "../components/login.css";
 import Dashboard from "./dashboard";
+import Kanban from './Kanban';
 
 const Home = () => {
   const { logOut, user } = useUserAuth();
@@ -40,6 +41,9 @@ const Home = () => {
         </Link>
         <Link to="/calendar" className="link">
           Calendar
+        </Link>
+        <Link to="/kanban" className="link">
+          Kanban
         </Link>
       </div>
     </>
